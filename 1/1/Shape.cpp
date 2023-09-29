c5ec5662605caedabdc5cf4b35f836e9f6d15608
Igor Kastyrkin <kastyrkinIV@ya.ru>
2023-09-29 14:51:55 +0300

    next commit

--- 1/1/Shape.cpp
+++ 1/1/Shape.cpp
@@ -7,49 +7,56 @@
 Shape::Shape(int _type, int p_[])
 {
 	type = _type;
-	// заполн¤ем координаты фигуры
+    const char idx = 3;
+    //расчитываем длину массива
 	switch (type)
 	{
 	case line:
-		p_length = 2*3;
+		p_length = 2*idx;
 		break;
 	case sqr:
-		p_length = 4*3;
+		p_length = 4*idx;
 		break;
 	case cube:
-		p_length = 8*3;
+		p_length = 8*idx;
 		break;
 	case circle:
-		p_length = 3*3;
+		p_length = 3*idx;
 		break;
 	case cylinder:
-		p_length = 4*3;
+		p_length = 4*idx;
 		break;
 	}
-
+	// заполн¤ем координаты фигуры
 	for (unsigned int i = 0; i < p_length; ++i)
 	{
 		p[i] = p_[i];
 	}
 
+
+    Square(type);
+    Volume(type);
 };
 
+// сторона а
 int Shape::sideA()
 {
 	return p[0] - p[3]; //8
 }
 
+// сторона Б
  int Shape::sideB()
 {
 	return p[1] - p[4]; //8
 }
 
+// сторона С
 int Shape::sideC()
 {
 	return p[2] - p[5]; //8
 }
 
-
+// площадь фигуры
  double Shape::Square(int type)
 {
 
@@ -76,25 +83,19 @@ int Shape::sideC()
 	return square;
 }
 
+// объем фигуры
 double Shape::Volume(int type)
 {
 	switch (type)
 	{
-	case line:
-		volume = 0;
-		break;
-	case sqr:
-		volume = 0;
-		break;
 	case cube:
 		volume = sideA() * sideB() * sideC();
-		break;
-	case circle:
-		volume = 0;
 		break;
 	case cylinder:
 		volume = M_PI * p[2] * p[2] * p[3];
 		break;
+	default:
+		volume = 0;
 	}
 	return volume;
 }
--- 1/1/Shape.h
+++ 1/1/Shape.h
@@ -11,28 +11,22 @@ public:
 
 	Shape() = default;
 	Shape(int type, int p[]);
-	Shape(int type, int _x1, int _y1, double R, double H);
 	int getType() { return type; }
 	double Square(int type);
 	double Volume(int type);
-	int sideA();
-	int sideB();
-	int sideC();
-	int sideS();
 	unsigned int p_length = 0;
 	int p[24];
 private:
-
+	int sideA();
+	int sideB();
+	int sideC();
 	static const int line = 0;
 	static const int sqr = 1;
 	static const int cube = 2;
 	static const int circle = 3;
 	static const int cylinder = 4;
 	int type;
+
 	double volume = 0;
 	double square = 0;
-	double height = 0;
-	double radius = 0;
-
-
 };
